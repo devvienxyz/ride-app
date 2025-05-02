7b417e4f9521a2da54e1b0d4a57623888d799e30
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-02 23:06:30 +0800

    Initialized user manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, phone_number, role="rider", password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone_number=phone_number,
+            role=role,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone_number, password):
+        user = self.create_user(email, first_name, last_name, phone_number, "admin", password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -20,6 +43,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name", "phone_number"]
+
+    objects = UserManager()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.role})"
