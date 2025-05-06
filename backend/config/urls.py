3f78b86a0e6e0131646682f0fc6660413356349e
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-06 19:47:29 +0800

    Added user admin

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/rides/", include("ride.urls")),
-    path("api/auth/", include("users.urls")),
 ]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id_user", "email", "first_name", "last_name", "role", "phone_number")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("role",)
+    ordering = ("id_user",)
+    list_per_page = 20
+    list_display_links = ("id_user", "email")
