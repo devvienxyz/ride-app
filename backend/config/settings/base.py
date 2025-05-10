998991776d705088352cc7b4b3b0e0a811c0d61b
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-11 02:21:18 +0800

    Use dj-database-url

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django_filters",
     "users",
     "ride",
+    "dj_database_url",
 ]
 
 MIDDLEWARE = [
@@ -69,15 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
