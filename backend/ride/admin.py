d97306e83f7de7e3091c0fd92e81c34716f92884
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-11 01:15:05 +0800

    Registered Ride app to admin

--- backend/ride/admin.py
+++ backend/ride/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from ride.models import Ride, RideEvent
 
-# Register your models here.
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_ride",
+        "status",
+        "id_rider",
+        "id_driver",
+        "pickup_time",
+    )
+    list_filter = ("status",)
+    search_fields = ("id_rider__email", "id_driver__email")
+    ordering = ("-pickup_time",)
+    autocomplete_fields = ("id_rider", "id_driver")
+
+
+@admin.register(RideEvent)
+class RideEventAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_ride_event",
+        "id_ride",
+        "description",
+        "created_at",
+    )
+    search_fields = ("description",)
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    autocomplete_fields = ("id_ride",)
+    list_per_page = 20
