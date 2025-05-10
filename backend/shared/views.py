92aec7452a278bd31e47f73cafd7a56c15c55af8
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-11 02:53:34 +0800

    Created custom public view class

---
+++ backend/shared/views.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class PublicView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        return Response({"message": "This is a public view!"})
