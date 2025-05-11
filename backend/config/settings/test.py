6ec65f22252defa1a0d54431a13d20ca66ddf919
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-11 11:33:23 +0800

    Test.py for test env

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -1,14 +1,21 @@
-from . import development as development_settings  # noqa: F401
+import os
+from .development import *  # noqa: F401, F403
 
 DEBUG = True
 ALLOWED_HOSTS = []  # No need for domain names in testing
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_db",  # Test database
-        "USER": "postgres",
-        "PASSWORD": "password",
+        "NAME": "ride_app_db",
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "localhost",
         "PORT": "5432",
+        "TEST": {
+            "NAME": "test_ride_app_db",  # explicit
+        },
     }
 }
+
+LOGGING_CONFIG = None
