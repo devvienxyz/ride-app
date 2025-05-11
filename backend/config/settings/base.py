2c94a20b1836381c6bc81c7578ea816077203d67
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-12 03:27:20 +0800

    Set simple jwt user id field to id_user

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -131,6 +131,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+SIMPLE_JWT = {
+    "USER_ID_FIELD": "id_user"
+}
+
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "shared.permissions.IsAdminOrSuperuser",
