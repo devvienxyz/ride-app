eda770fd268655928c3b8fab117070a26156c3fb
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-12 23:32:37 +0800

    Check response data keys on test_login_success

--- backend/users/tests/test_views.py
+++ backend/users/tests/test_views.py
@@ -7,9 +7,7 @@ def test_login_success(client, create_regular_user):
     url = reverse("token_obtain_pair")
     resp = client.post(url, {"email": create_regular_user.email, "password": "password"}, format="json")
     assert resp.status_code == 200
-    assert "access" in resp.data
-    assert "refresh" in resp.data
-    assert resp.data["email"] == create_regular_user.email
+    assert set(resp.data.keys()) == {"access", "refresh"}
 
 
 @pytest.mark.django_db
@@ -18,7 +16,8 @@ def test_login_invalid_credentials(client):
     resp = client.post(url, {"email": "wrong@example.com", "password": "wrongpass"}, format="json")
     assert resp.status_code == 401
     assert "access" not in resp.data
-    assert "Invalid email or password."  == str(resp.data["detail"])
+    assert "Invalid email or password." == str(resp.data["detail"])
+
 
 def test_login_missing_fields(client):
     url = reverse("token_obtain_pair")
