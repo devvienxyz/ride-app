8cc68fa3dc03a3e4297538d386b6da50faa65b6d
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-12 14:14:49 +0800

    Use login form component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import LoginForm from "@components/login-form";
+
+
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Ride Sharing App</h1>
       <p>Find your next ride!</p>
+
+      <LoginForm />
     </div>
   );
 }
