34e5d5a5e66448b4c765aab37671ade96fc55b07
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-12 21:15:49 +0800

    Set default pagination class

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -144,5 +144,6 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         # Exclude the browsable UI by default
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
