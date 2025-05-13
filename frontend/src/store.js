e79584d22e43b17e49293bc6295e5cf44bc3081c
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-13 17:37:56 +0800

    Persist state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,22 @@
 import { create } from "zustand"
+import { persist } from "zustand/middleware"
 
-const useStore = create((set) => ({
+const initialState = {
   user: null,
-  setUser: (user) => set({ user }),
-  logout: () => set({ user: null }),
   rides: [],
-  setRides: (rides) => set({ rides }),
-}))
+}
+
+const useStore = create(
+  persist(
+    (set) => ({
+      ...initialState,
+      setUser: (user) => set({ user }),
+      logout: () => set(initialState),
+      setRides: (rides) => set({ rides }),
+    }),
+    {}
+  )
+)
 
 export default useStore
+export { initialState }
