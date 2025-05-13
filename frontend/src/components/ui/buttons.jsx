986fa1a567a7c6ddbb9a4d0ed49f5eccd367471e
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-13 14:39:57 +0800

    Pass button props

--- frontend/src/components/ui/buttons.jsx
+++ frontend/src/components/ui/buttons.jsx
@@ -1,5 +1,5 @@
 const BrandButton = ({ children, ...props }) => (
-  <button className="brand w-full bg-blue-600 text-xl text-white disabled:bg-blue-100 rounded mt-4 py-4">
+  <button className="brand w-full bg-blue-600 text-xl text-white disabled:bg-blue-100 rounded mt-4 py-4" {...props}>
     {children}
   </button>
 )
