92564ac2d96d5c1cc87d4fb01c115533671cb7c8
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-15 21:19:01 +0800

    Layout styles -- soft

--- frontend/src/components/ui/layout.jsx
+++ frontend/src/components/ui/layout.jsx
@@ -1,8 +1,7 @@
-import axios from "axios";
 import useStore from "@/store"
+import axiosInstance from "@/axios";
 import { LeanButton } from "./buttons"
 import { BrandLink } from "./typography";
-import axiosInstance from "@/axios";
 
 const NavBar = () => {
   const { logout } = useStore((state) => state)
@@ -18,25 +17,24 @@ const NavBar = () => {
   }
 
   return (
-    <nav className="overflow-x-hidden absolute top-0 px-6 py-4 w-full bg-blue-800">
-      <ul className="flex flex-row justify-between">
+    <nav className="overflow-x-hidden absolute top-0 px-4 sm:px-12 py-4 w-full bg-white">
+      <ul className="sm:mx-12 flex flex-row justify-between">
         <li>
-          <BrandLink addlClasses="text-2xl text-white" to="/">ride</BrandLink>
+          <BrandLink addlClasses="text-2xl text-blue-950" to="/">ride</BrandLink>
         </li>
         <li className="self-center">
-          <LeanButton onClick={handleLogout} addlClasses="text-[1rem]">Log out</LeanButton>
+          <LeanButton onClick={handleLogout} addlClasses="text-[1rem] text-blue-950">Log out</LeanButton>
         </li>
       </ul>
     </nav>
   )
-
 }
 export default function Layout({ children }) {
   const { user } = useStore((state) => state)
   const addlClasses = !!user ? "mt-16" : "";
 
   return (
-    <div className={`w-screen h-screen overflow-x-hidden mx-auto ${addlClasses}`}>
+    <div className={`w-screen h-screen overflow-x-hidden mx-auto text-blue-950 bg-blue-50 ${addlClasses}`}>
       {user && <NavBar />}
       {children}
     </div>
