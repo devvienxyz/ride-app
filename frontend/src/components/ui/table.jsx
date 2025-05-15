d477deff17d2040836b383d771675346d1320420
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-15 21:20:44 +0800

    Use wip table component on rides and rideevents component

--- frontend/src/components/ui/table.jsx
+++ frontend/src/components/ui/table.jsx
@@ -3,16 +3,52 @@ import { ChevronLeftButton, ChevronRightButton, LeanButton } from "./buttons";
 
 function TableCell({ children, addlClasses = "", ...props }) {
   return (
-    <div className={`table-col-span-6 table-cell bg-blue-100 border border-blue-50 whitespace-nowrap px-1 py-4 ${addlClasses}`} {...props}>
-      {children}
-    </div>
+    // <div className={`table-col-span-6 table-cell bg-blue-100 border border-blue-50 whitespace-nowrap px-1 py-4 ${addlClasses}`} {...props}>
+    //   {children}
+    // </div>
+
+    <td class="p-4 border-b border-slate-200 py-5" {...props}>
+      <p class="block font-semibold text-sm text-slate-800">{children}</p>
+    </td>
   )
 }
 
 function TableRow({ children, addlClasses = "", ...props }) {
   return (
-    <div className={`table-row ${addlClasses}`}>
+    // <div className={`table-row ${addlClasses}`}>
+    //   {children}
+    // </div>
+    <tr class="hover:bg-slate-50">
       {children}
+    </tr>
+  )
+}
+
+function TableSearchBar({ title, subTitle, searchPlaceholder, ...props }) {
+  return (
+    <div class="w-full flex justify-between items-center mb-3 mt-1 pl-3">
+      <div>
+        <h3 class="text-lg font-semibold text-slate-800">{title}</h3>
+        <p class="text-slate-500">{subTitle}</p>
+      </div>
+      <div class="ml-3">
+        <div class="w-full max-w-sm min-w-[200px] relative">
+          <div class="relative">
+            <input
+              class="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+              placeholder={searchPlaceholder}
+            />
+            <button
+              class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
+              type="button"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-8 h-8 text-slate-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -21,30 +57,7 @@ export default function Table({ resourceName, headers, paginationCtx, rowRendere
 
   return (
     <div>
-      <div class="w-full flex justify-between items-center mb-3 mt-1 pl-3">
-        <div>
-          <h3 class="text-lg font-semibold text-slate-800">Generated Invoice for this Month</h3>
-          <p class="text-slate-500">Overview of the invoices.</p>
-        </div>
-        <div class="ml-3">
-          <div class="w-full max-w-sm min-w-[200px] relative">
-            <div class="relative">
-              <input
-                class="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
-                placeholder="Search for invoice..."
-              />
-              <button
-                class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
-                type="button"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-8 h-8 text-slate-600">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TableSearchBar />
 
       <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
         <table class="w-full text-left table-auto min-w-max">
--- frontend/src/pages/Rides.jsx
+++ frontend/src/pages/Rides.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import useStore from "@/store"
 import axiosInstance from "@/axios"
-import { Table } from "@components/ui";
+import { Table, TableCell, TableRow } from "@components/ui";
 
 const RIDE_TABLE_HEADERS = [
   "Status",
@@ -16,14 +16,14 @@ function renderTableRow(rowData, rowIdx) {
   const { status, rider, driver, pickup_loc, dropoff_loc, pickup_time } = rowData;
 
   return (
-    <tr key={`ride-${rowIdx}`}>
-      <td className="whitespace-nowrap">{status}</td>
-      <td className="whitespace-nowrap">{rider}</td>
-      <td className="whitespace-nowrap">{driver}</td>
-      <td className="whitespace-nowrap">{pickup_loc}</td>
-      <td className="whitespace-nowrap">{dropoff_loc}</td>
-      <td className="whitespace-nowrap">{pickup_time}</td>
-    </tr>
+    <TableRow key={`ride-${rowIdx}`}>
+      <TableCell>{status}</TableCell>
+      <TableCell>{rider}</TableCell>
+      <TableCell>{driver}</TableCell>
+      <TableCell>{pickup_loc}</TableCell>
+      <TableCell>{dropoff_loc}</TableCell>
+      <TableCell>{pickup_time}</TableCell>
+    </TableRow>
   )
 }
 
@@ -31,11 +31,16 @@ export default function Rides() {
   const { rides, setRides } = useStore((state) => state)
 
   useEffect(() => {
-    // Fetch rides when the component mounts
     const fetchRides = async () => {
       try {
-        const response = await axiosInstance.get("/rides/", { withCredentials: true });
-        setRides(response.data || []); // Update Zustand state
+        const { data } = await axiosInstance.get("/rides/", { withCredentials: true });
+        const ctx = {
+          count: data?.count || 0,
+          next: data?.next || null,
+          previous: data?.previous || null,
+          results: data?.results || [],
+        }
+        setRides(ctx);
       } catch (error) {
         // console.error("Error fetching rides", error);
         // Handle error (show error message)
@@ -47,9 +52,13 @@ export default function Rides() {
 
   return (
     <div className="w-full flex flex-row justify-center self-center">
-      <div className="py-6 w-full xl:max-w-1/2 gap-6">
+      <div className="py-6 w-full xl:max-w-2/3 gap-6">
         <div className="flex flex-row justify-center w-full">
-          <Table resourceName="rides" headers={RIDE_TABLE_HEADERS} data={rides} rowRenderer={renderTableRow} />
+          <Table
+            resourceName="rides"
+            headers={RIDE_TABLE_HEADERS}
+            paginationCtx={rides}
+            rowRenderer={renderTableRow} />
         </div>
       </div>
     </div>
