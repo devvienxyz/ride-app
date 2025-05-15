a623d8a3bf3d81de2187b4e4dd1396a68e67e85b
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-15 20:53:52 +0800

    Chevron buttons

--- frontend/src/components/ui/buttons.jsx
+++ frontend/src/components/ui/buttons.jsx
@@ -1,12 +1,14 @@
+import { ChevronLeft, ChevronRight } from "@icons";
+
 const LeanButton = ({ children, addlClasses = "", ...props }) => {
-  const baseClasses = "w-full bg-transparent disabled:text-gray-500 text-xl text-white rounded m-0 p-0";
+  const baseClasses = "w-full bg-transparent disabled:text-gray-500 text-xl rounded m-0 p-0";
   const classes = `${baseClasses} ${addlClasses}`;
 
   return <button className={classes} {...props}>{children}</button>;
 };
 
 const Button = ({ children, addlClasses = "", ...props }) => {
-  const baseClasses = "w-full bg-blue-600 disabled:bg-blue-100 text-xl text-white rounded mt-4 py-4";
+  const baseClasses = "w-full flex justify-center bg-blue-600 disabled:bg-blue-100 text-xl text-white rounded mt-4 py-4";
   const classes = `${baseClasses} ${addlClasses}`;
 
   return <button className={classes} {...props}>{children}</button>;
@@ -18,4 +20,16 @@ const BrandButton = ({ children, ...props }) => (
   </Button>
 )
 
-export { Button, BrandButton, LeanButton }
+const ChevronLeftButton = ({ props }) => (
+  <Button addlClasses="bg-transparent" {...props}>
+    <ChevronLeft stroke="oklch(28.2% 0.091 267.935)" />
+  </Button>
+)
+
+const ChevronRightButton = ({ props }) => (
+  <Button addlClasses="bg-transparent" {...props}>
+    <ChevronRight stroke="oklch(28.2% 0.091 267.935)" />
+  </Button>
+)
+
+export { Button, BrandButton, LeanButton, ChevronLeftButton, ChevronRightButton }
