a405ec909eb7951ab577585185190ab45d624aff
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-18 13:42:04 +0800

    Use Sidebar component in detail

--- frontend/src/components/rides/detail.jsx
+++ frontend/src/components/rides/detail.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react"
+import { Button } from "@components/ui"
+import axiosInstance from "@/axios";
+import Sidebar from "../ui/sidebar";
 
 function DetailItem({ label, children }) {
   return (
@@ -12,10 +15,19 @@ function DetailItem({ label, children }) {
 export function SideDialog({ rideId, onClose }) {
   const [details, setDetails] = useState(null);
 
+  const handleEdit = async () => {
+    console.log("edit")
+  }
+
+  const handleDelete = async () => {
+    console.log("delete")
+    const res = await axiosInstance.delete(`/rides/${rideId}/`, { withCredentials: true });
+
+  }
+
   useEffect(() => {
     async function fetchDetails() {
-      const res = await fetch(`/api/rides/${rideId}`);
-      const data = await res.json();
+      const { data } = await axiosInstance.get(`/rides/${rideId}/`, { withCredentials: true });
       setDetails(data);
     }
 
@@ -25,50 +37,55 @@ export function SideDialog({ rideId, onClose }) {
   if (!details) return <div className="fixed z-2 right-0 top-0 w-full sm:w-2/3 lg:w-1/3 xl:max-w-1/4 h-full bg-slate-50 shadow p-8 pt-12 flex flex-col">Loading...</div>;
 
   return (
-    <div className="fixed z-2 right-0 top-0 w-full sm:w-2/3 lg:w-1/3 xl:max-w-1/4 h-full bg-slate-50 shadow p-8 pt-12 flex flex-col text-slate-800">
-      <button onClick={onClose} className="text-slate-900 self-end mb-2">Close</button>
+    <Sidebar isLoading={!details} onClose={onClose}>
+      <div className="flex flex-col h-full justify-between">
+        <div className="flex flex-col gap-6">
+          <DetailItem label="Ride ID">
+            <p>{details.id_ride}</p>
+          </DetailItem>
 
-      <div className="flex flex-col gap-6">
-        <DetailItem label="Ride ID">
-          <p>{details.id_ride}</p>
-        </DetailItem>
+          <DetailItem label="Status">
+            <p>{details.status}</p>
+          </DetailItem>
 
-        <DetailItem label="Status">
-          <p>{details.status}</p>
-        </DetailItem>
+          <DetailItem label="Pickup Time">
+            <p>{details.pickup_time}</p>
+          </DetailItem>
 
-        <DetailItem label="Pickup Time">
-          <p>{details.pickup_time}</p>
-        </DetailItem>
+          <DetailItem label="Pickup Location">
+            <p>{details.pickup_latitude}, {details.pickup_longitude}</p>
+          </DetailItem>
 
-        <DetailItem label="Pickup Location">
-          <p>{details.pickup_latitude}, {details.pickup_longitude}</p>
-        </DetailItem>
+          <DetailItem label="Dropoff Location">
+            <p>{details.dropoff_latitude}, {details.dropoff_longitude}</p>
+          </DetailItem>
 
-        <DetailItem label="Dropoff Location">
-          <p>{details.dropoff_latitude}, {details.dropoff_longitude}</p>
-        </DetailItem>
+          <DetailItem label="Driver">
+            <p>{details.id_driver.first_name} {details.id_driver.last_name}</p>
+            <p>{details.id_driver.email}</p>
+          </DetailItem>
 
-        <DetailItem label="Driver">
-          <p>{details.id_driver.first_name} {details.id_driver.last_name}</p>
-          <p>{details.id_driver.email}</p>
-        </DetailItem>
+          <DetailItem label="Rider">
+            <p>{details.id_rider.first_name} {details.id_rider.last_name}</p>
+            <p>{details.id_rider.email}</p>
+          </DetailItem>
 
-        <DetailItem label="Rider">
-          <p>{details.id_rider.first_name} {details.id_rider.last_name}</p>
-          <p>{details.id_rider.email}</p>
-        </DetailItem>
+          <DetailItem label="Ride Events">
+            <ul className="overflow-auto max-h-64">
+              {details.todays_ride_events?.map(event => (
+                <li key={event.id_ride_event}>
+                  {new Date(event.created_at).toLocaleString()}: {event.description}
+                </li>
+              ))}
+            </ul>
+          </DetailItem>
+        </div>
 
-        <DetailItem label="Ride Events">
-          <ul className="overflow-auto max-h-64">
-            {details.todays_ride_events?.map(event => (
-              <li key={event.id_ride_event}>
-                {new Date(event.created_at).toLocaleString()}: {event.description}
-              </li>
-            ))}
-          </ul>
-        </DetailItem>
+        <div className="flex justify-center gap-6">
+          <Button onClick={handleEdit} addlClasses="!text-gray-800 text-md border border-slate-900 hover:border-blue-800 hover:bg-blue-800 hover:!text-white bg-transparent">Edit</Button>
+          <Button onClick={handleDelete} addlClasses="!text-gray-800 text-md border border-slate-900 hover:border-red-800 hover:bg-red-800 hover:!text-white bg-transparent">Delete</Button>
+        </div>
       </div>
-    </div>
+    </Sidebar>
   );
 }
