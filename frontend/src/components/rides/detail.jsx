ef5a230026b7090025e6d84d58c0dcf12207e412
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-22 01:45:46 +0800

    Justify-start instead of justify-center styling on detail component

--- frontend/src/components/rides/detail.jsx
+++ frontend/src/components/rides/detail.jsx
@@ -44,49 +44,47 @@ export function DetailSidebar({ rideId, onClose }) {
 
   return (
     <Sidebar onClose={onClose}>
-      <div className="flex flex-col h-full justify-between">
-        <div className="flex flex-col gap-6">
-          <DetailItem label="Ride ID">
-            <p>{details.id_ride}</p>
-          </DetailItem>
+      <div className="flex flex-col h-full justify-start gap-6">
+        <DetailItem label="Ride ID">
+          <p>{details.id_ride}</p>
+        </DetailItem>
 
-          <DetailItem label="Status">
-            <p>{details.status}</p>
-          </DetailItem>
+        <DetailItem label="Status">
+          <p>{details.status}</p>
+        </DetailItem>
 
-          <DetailItem label="Pickup Time">
-            <p>{details.pickup_time}</p>
-          </DetailItem>
+        <DetailItem label="Pickup Time">
+          <p>{details.pickup_time}</p>
+        </DetailItem>
 
-          <DetailItem label="Pickup Location">
-            <p>{details.pickup_latitude}, {details.pickup_longitude}</p>
-          </DetailItem>
+        <DetailItem label="Pickup Location">
+          <p>{details.pickup_latitude}, {details.pickup_longitude}</p>
+        </DetailItem>
 
-          <DetailItem label="Dropoff Location">
-            <p>{details.dropoff_latitude}, {details.dropoff_longitude}</p>
-          </DetailItem>
+        <DetailItem label="Dropoff Location">
+          <p>{details.dropoff_latitude}, {details.dropoff_longitude}</p>
+        </DetailItem>
 
-          <DetailItem label="Driver">
-            <p>{details.id_driver.first_name} {details.id_driver.last_name}</p>
-            <p>{details.id_driver.email}</p>
-          </DetailItem>
+        <DetailItem label="Driver">
+          <p>{details.id_driver.first_name} {details.id_driver.last_name}</p>
+          <p>{details.id_driver.email}</p>
+        </DetailItem>
 
-          <DetailItem label="Rider">
-            <p>{details.id_rider.first_name} {details.id_rider.last_name}</p>
-            <p>{details.id_rider.email}</p>
-          </DetailItem>
+        <DetailItem label="Rider">
+          <p>{details.id_rider.first_name} {details.id_rider.last_name}</p>
+          <p>{details.id_rider.email}</p>
+        </DetailItem>
 
-          <DetailItem label="Ride Events Today">
-            <ul className="overflow-auto max-h-64">
-              {details.todays_ride_events?.map(event => (
-                <li key={event.id_ride_event}>
-                  {new Date(event.created_at).toLocaleString()}: {event.description}
-                </li>
-              ))}
-            </ul>
-            {!details?.todays_ride_events?.length && "None"}
-          </DetailItem>
-        </div>
+        <DetailItem label="Ride Events Today">
+          <ul className="overflow-auto max-h-64">
+            {details.todays_ride_events?.map(event => (
+              <li key={event.id_ride_event}>
+                {new Date(event.created_at).toLocaleString()}: {event.description}
+              </li>
+            ))}
+          </ul>
+          {!details?.todays_ride_events?.length && "None"}
+        </DetailItem>
 
         <div className="flex flex-col sm:flex-row justify-center gap-2 sm:gap-6 md:px-24 lg:px-20">
           <button onClick={handleRenderEdit} className="py-2 px-4 md:w-full text-gray-800 text-md border rounded-md border-slate-900 bg-transparent hover:border-slate-500 hover:bg-slate-500 hover:text-white">Edit</button>
--- frontend/src/components/rides/update-form.jsx
+++ frontend/src/components/rides/update-form.jsx
@@ -43,7 +43,6 @@ export default function RideEditForm({ currentValues, rideId, setIsEditing }) {
   return (
     <form>
       <div className="flex flex-col gap-6">
-
         <SingleSelectOnDropdown
           name="status"
           options={StatusFilterOptions}
