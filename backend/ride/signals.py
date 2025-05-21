91b813bbc4a5ea22b94e7126b1b8979516004ee1
Vivien Bunane <devvien.xyz@gmail.com>
2025-05-22 01:01:42 +0800

    Signal to create ride event on status change and on create ride instance

--- backend/ride/signals.py
+++ backend/ride/signals.py
@@ -1,1 +1,20 @@
-# TODO: Update ride status on create of a ride event
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import RideEvent, Ride
+
+
+@receiver(post_save, sender=Ride)
+def create_event_on_create_and_status_change(sender, instance, created, **kwargs):
+    # Creates a ride event on:
+    #   - create of ride
+    #   - change of ride status
+
+    if created:
+        RideEvent.objects.create(ride=instance, description=f"Ride created with initial status '{instance.status}'")
+        return
+
+    # only log the event if status changed
+    if hasattr(instance, "_original_status") and instance.status != instance._original_status:
+        RideEvent.objects.create(
+            ride=instance, description=f"Ride status changed from '{instance._original_status}' to '{instance.status}'"
+        )
